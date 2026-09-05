3674107580e116c172efa97f6ba32b0093907a69
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-09-05 14:49:10 +0530

    Integrate Clerk authentication by adding ClerkProvider and updating App component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+
+
+
+//Import Your Published key
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!PUBLISHABLE_KEY){
+    throw new Error("Missing CLERK Publishable Key");
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
